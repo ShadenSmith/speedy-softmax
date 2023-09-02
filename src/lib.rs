50338099d29f21a5718a691b7fe08c855b2cccb3
Shaden Smith <ShadenTSmith@gmail.com>
2023-09-02 03:22:39 -0700

    Repo setup (#1)

---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+use candle_core::{Result, Tensor};
+
+/// Applies the softmax function to the input tensor, rescaling the element so that elements on
+/// a slice of fixed index on dimension `dim` are between 0 and 1 and sum to 1.
+///
+/// ```rust
+/// use candle_core::{Tensor, Device, test_utils::to_vec2_round};
+/// let a = Tensor::new(&[[0f32, 1., 0., 1.], [-2., 2., 3., -3.]], &Device::Cpu)?;
+/// let a = speedy_softmax::fused_softmax(&a, 1)?;
+/// assert_eq!(
+///     to_vec2_round(&a, 4)?,
+///     &[
+///         [0.1345, 0.3655, 0.1345, 0.3655],
+///         [0.0049, 0.2671, 0.7262, 0.0018]
+///     ]);
+/// # Ok::<(), candle_core::Error>(())
+/// ```
+pub fn fused_softmax<D: candle_core::shape::Dim>(xs: &Tensor, dim: D) -> Result<Tensor> {
+    let dim = dim.to_index(xs.shape(), "fused-softmax")?;
+    let max = xs.max_keepdim(dim)?;
+    let diff = xs.broadcast_sub(&max)?;
+    let num = diff.exp()?;
+    let den = num.sum_keepdim(dim)?;
+    num.broadcast_div(&den)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use candle_core::test_utils::to_vec2_round;
+    use candle_core::Device;
+
+    #[test]
+    fn test_fused_softmax() {
+        let input = Tensor::rand(-1.0f32, 1.0f32, &[6, 34], &Device::Cpu).unwrap();
+
+        let base_output = candle_nn::ops::softmax(&input, 1).unwrap();
+        let test_output = fused_softmax(&input, 1).unwrap();
+
+        assert_eq!(
+            to_vec2_round(&base_output, 4).unwrap(),
+            to_vec2_round(&test_output, 4).unwrap(),
+        );
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,58 @@
+use candle_core::{Device, Result, Tensor};
+use clap::Parser;
+
+use std::time::Instant;
+
+use speedy_softmax::fused_softmax;
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(default_value_t = 128)]
+    /// Batch size of the input.
+    batch_size: usize,
+
+    #[arg(default_value_t = 1024)]
+    /// Number of input features.
+    hidden_dim: usize,
+
+    #[arg(default_value_t = 1000)]
+    /// Number of times to repeat the benchmark.
+    reps: usize,
+}
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+
+    let gen_input = || {
+        Tensor::rand(
+            -1.0f32,
+            1.0f32,
+            &[args.batch_size, args.hidden_dim],
+            &Device::Cpu,
+        )
+    };
+
+    let input = gen_input()?;
+    let start = Instant::now();
+    for _ in 0..args.reps {
+        let _ = candle_nn::ops::softmax(&input, 1)?;
+    }
+    let base_time = start.elapsed().as_secs_f64() / args.reps as f64;
+
+    let input = gen_input()?;
+    let start = Instant::now();
+    for _ in 0..args.reps {
+        let _ = fused_softmax(&input, 1)?;
+    }
+    let fused_time = start.elapsed().as_secs_f64() / args.reps as f64;
+
+    println!(
+        "Softmax: hidden: {}, batch: {}",
+        args.hidden_dim, args.batch_size
+    );
+    println!("Base: {:.4} ms", base_time * 1000.0);
+    println!("Fused: {:.4} ms", fused_time * 1000.0);
+    println!("Speedup: {:.2}x", base_time / fused_time);
+
+    Ok(())
+}
