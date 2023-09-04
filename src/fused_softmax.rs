09c53dc82f8252d2dfa7d5ec305636c4ffefe481
Shaden Smith <shadentsmith@gmail.com>
2023-09-04 11:42:13 -0700

    softmax slice test

--- src/fused_softmax.rs
+++ src/fused_softmax.rs
@@ -25,3 +25,28 @@ pub fn softmax_slice(input: &[f32], output: &mut [f32]) {
 
     output.iter_mut().for_each(|o| *o *= denominator);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use crate::tests::common::round4;
+
+    #[test]
+    fn test_softmax() {
+        let xs: Vec<f32> = vec![
+            -0.8414, 0.7612, -0.7832, 0.7465, -0.1200, -2.6573, 0.4870, -1.2291,
+        ];
+
+        let expected: Vec<f32> = vec![
+            0.0538, 0.2671, 0.0570, 0.2632, 0.1107, 0.0088, 0.2030, 0.0365,
+        ];
+
+        let mut test = vec![0.; xs.len()];
+        softmax_slice(&xs, &mut test);
+
+        for (mine, gold) in test.into_iter().zip(expected) {
+            assert_eq!(round4(mine), round4(gold));
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,16 @@
 pub mod candle;
 pub mod fused_softmax;
+
+#[cfg(test)]
+mod tests {
+    // Setup some testing utilities
+
+    pub mod common {
+        use rand::{thread_rng, Rng};
+
+        /// Round a float to 4 decimal places.
+        pub fn round4(x: f32) -> f32 {
+            (x * 10000.0).round() / 10000.0
+        }
+    }
+}
